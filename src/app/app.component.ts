c4182dd5b973c997da804713568a2c0dbb3a6920
DESKTOP-1JGOD95\APEIRON 2.0 <veljko.mijovic@hypercubesoft.com>
2024-10-11 16:14:41 +0200

    Header done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BackgroundComponent } from "./components/background/background.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { GetInTouchMarqeeComponent } from "./components/get-in-touch-marqee/get-in-touch-marqee.component";
+import { NgxMarqueeModule } from 'ngx-marquee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BackgroundComponent, HeaderComponent, GetInTouchMarqeeComponent, NgxMarqueeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/get-in-touch-marqee/get-in-touch-marqee.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgxMarqueeModule } from 'ngx-marquee';
+
+@Component({
+  selector: 'app-get-in-touch-marqee',
+  standalone: true,
+  imports: [NgxMarqueeModule],
+  templateUrl: './get-in-touch-marqee.component.html',
+  styleUrl: './get-in-touch-marqee.component.css'
+})
+export class GetInTouchMarqeeComponent {
+
+}
